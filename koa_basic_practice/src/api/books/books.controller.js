04107c1c31c38f32e8119baac5ae9cfd1a96ef28
DDangDin <kcorelec7123@gmail.com>
2023-06-16 00:32:54 +0900

    add practice file

---
+++ koa_basic_practice/src/api/books/books.controller.js
@@ -0,0 +1,19 @@
+﻿exports.list = (ctx) => {
+    ctx.body = 'listed'
+}
+
+exports.create = (ctx) => {
+    ctx.body = 'created'
+}
+
+exports.delete = (ctx) => {
+    ctx.body = 'deleted'
+}
+
+exports.replace = (ctx) => {
+    ctx.body = 'replaced'
+}
+
+exports.update = (ctx) => {
+    ctx.body = 'updated'
+}
